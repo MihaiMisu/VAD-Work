ddc9437d3a19e58dbc17acf97f64accaaf862782
MihaiMisu <33182286+MihaiMisu@users.noreply.github.com>
2017-10-29 09:48:51 +0200

    Delete wav_header.h

--- wav_header.h
+++
@@ -1,7 +0,0 @@
-#ifndef WAV_HEADER_H_   /* Include guard */
-#define WAV_HEADER_H_
-
-int wav_read(char path[], char file_name[]);  /* An example function declaration */
-void wav_read2(char path[], char file_name[], int **wav_pointer_array, int **wav_length, int **samp_frec);
-
-#endif // FOO_H_
